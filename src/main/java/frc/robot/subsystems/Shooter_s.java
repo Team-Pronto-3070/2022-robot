f916f3fcc8c0ac85694e8b8e8b0713d38a80d115
Ben <66845228+benfeinberg@users.noreply.github.com>
2022-03-02 16:38:12 -0800

    initial state space shooter

--- src/main/java/frc/robot/subsystems/Shooter_s.java
+++ src/main/java/frc/robot/subsystems/Shooter_s.java
@@ -4,33 +4,32 @@
 
 package frc.robot.subsystems;
 
-//import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-//import edu.wpi.first.math.MathUtil;
-//import edu.wpi.first.math.controller.BangBangController;
-//import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.controller.LinearQuadraticRegulator;
+import edu.wpi.first.math.estimator.KalmanFilter;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.LinearSystemLoop;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-/**
- * Shooter Subsystem
- * 
- * Handles all the motors in the shooter
- */
 public class Shooter_s extends SubsystemBase {
 
   private WPI_TalonFX tal_Shooter;
-//  private BangBangController shooter_bb;
-//  private PIDController shooter_PID;
-//  private SimpleMotorFeedforward shooter_ff;
   private double shooterRPM = Constants.SHOOTER.DEFAULT_SHOOTER_RPM;
+
+  private final LinearSystem<N1, N1, N1> shooter_plant;
+  private final KalmanFilter<N1, N1, N1> shooter_observer;
+  private final LinearQuadraticRegulator<N1, N1, N1> shooter_controller;
+  private final LinearSystemLoop<N1, N1, N1> shooter_loop;
   
 
   public Shooter_s() {
@@ -42,14 +41,27 @@ public class Shooter_s extends SubsystemBase {
     tal_Shooter.setInverted(true);
     tal_Shooter.configOpenloopRamp(Constants.SHOOTER.RAMP_TIME);
     tal_Shooter.configPeakOutputReverse(0);
-    tal_Shooter.config_kP(0, Constants.SHOOTER.PID.P);
-    tal_Shooter.config_kI(0, Constants.SHOOTER.PID.I);
-    tal_Shooter.config_kD(0, Constants.SHOOTER.PID.D);
-    tal_Shooter.config_kF(0, Constants.SHOOTER.FEEDFORWARD.V);
     
-//    shooter_bb = new BangBangController();
-//    shooter_PID = new PIDController(Constants.SHOOTER.PID.P, Constants.SHOOTER.PID.I, Constants.SHOOTER.PID.D);
-//    shooter_ff = new SimpleMotorFeedforward(Constants.SHOOTER.FEEDFORWARD.S, Constants.SHOOTER.FEEDFORWARD.V, Constants.SHOOTER.FEEDFORWARD.A);
+    shooter_plant = LinearSystemId.identifyVelocitySystem(Constants.SHOOTER.FEEDFORWARD.V, Constants.SHOOTER.FEEDFORWARD.A);
+
+    shooter_observer =
+      new KalmanFilter<>(
+          Nat.N1(),
+          Nat.N1(),    
+          shooter_plant,
+          VecBuilder.fill(3.0), // How accurate we think our model is
+          VecBuilder.fill(0.01), // How accurate we think our encoder data is
+          0.020);
+    
+    shooter_controller =
+      new LinearQuadraticRegulator<>(
+          shooter_plant,
+          VecBuilder.fill(8.0), // qelms. Velocity error tolerance, in radians per second. Decrease this to more heavily penalize state excursion, or make the controller behave more aggressively
+          VecBuilder.fill(12.0), // relms. Control effort (voltage) tolerance. Decrease this to more heavily penalize control effort, or make the controller less aggressive. 12 is a good starting point because that is the (approximate) maximum voltage of a battery.
+          0.020); // Nominal time between loops. 0.020 for TimedRobot, but can be lower if using notifiers.
+
+    shooter_loop =
+      new LinearSystemLoop<>(shooter_plant, shooter_controller, shooter_observer, 12.0, 0.020);
 
     SmartDashboard.putNumber("shooter rpm", Constants.SHOOTER.DEFAULT_SHOOTER_RPM);
   }
@@ -63,10 +75,10 @@ public class Shooter_s extends SubsystemBase {
   }
 
   public void setRPM(double rpm) {
-//    set(shooter_bb.calculate(getRPM(), rpm));
-//    set(MathUtil.clamp(shooter_PID.calculate(getRPM(), rpm), 0, 1));
-    tal_Shooter.set(ControlMode.Velocity, rpm * 2048 / 600,
-                    DemandType.ArbitraryFeedForward, Constants.SHOOTER.FEEDFORWARD.S / 12);
+    shooter_loop.setNextR(VecBuilder.fill(Units.rotationsPerMinuteToRadiansPerSecond(rpm)));
+    shooter_loop.correct(VecBuilder.fill(Units.rotationsPerMinuteToRadiansPerSecond(getRPM())));
+    shooter_loop.predict(0.020);
+    tal_Shooter.setVoltage(shooter_loop.getU(0));
   }
 
   public void setRPM() {
@@ -78,6 +90,9 @@ public class Shooter_s extends SubsystemBase {
   }
 
   public void stop() {
+    shooter_loop.setNextR(VecBuilder.fill(0));
+    shooter_loop.correct(VecBuilder.fill(Units.rotationsPerMinuteToRadiansPerSecond(getRPM())));
+    shooter_loop.predict(0.020);
     tal_Shooter.set(0);
   }
 
@@ -86,4 +101,3 @@ public class Shooter_s extends SubsystemBase {
     SmartDashboard.putNumber("current shooter rpm", getRPM());
   }
 }
-
